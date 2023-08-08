c7955013c86cf7a374200dc26f19d746f28b3969
rathewolf <1132053+rahil627@users.noreply.github.com>
2023-08-07 23:37:10 -0700

    Create Main.hx

---
+++ code/shmup/heaps/Main.hx
@@ -0,0 +1,13 @@
+class Main extends hxd.App {
+    override function init() {
+        // Load assets, initialize game objects, and set up input handling here
+    }
+
+    override function update(dt: Float) {
+        // Update game logic, handle collisions, and manage game state
+    }
+
+    static function main() {
+        new Main();
+    }
+}
