65b35eaaad6c9b0fe832162db75ada3a8e61cbb9
rathewolf <1132053+rahil627@users.noreply.github.com>
2023-08-07 23:37:32 -0700

    Create Player.hx

---
+++ code/shmup/heaps/Player.hx
@@ -0,0 +1,10 @@
+class Player extends h2d.Sprite {
+    public function new() {
+        super(Assets.getTexture("player.png"));
+        // Set initial position, speed, and other properties
+    }
+
+    public function update(dt: Float) {
+        // Handle player movement and shooting
+    }
+}
