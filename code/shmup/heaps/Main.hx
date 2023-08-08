af7754fa5c95352adfdd1d9e36ff2efc1faf290f
rathewolf <1132053+rahil627@users.noreply.github.com>
2023-08-07 23:40:22 -0700

    Update Main.hx

--- code/shmup/heaps/Main.hx
+++ code/shmup/heaps/Main.hx
@@ -4,8 +4,28 @@ class Main extends hxd.App {
     }
 
     override function update(dt: Float) {
-        // Update game logic, handle collisions, and manage game state
+    // Update game logic, handle collisions, and manage game state
+
+    game.update(dt);
+
+    // Check for player input and update player(s)
+    if (Input.isKeyDown(hxd.Key.W)) {
+        players[0].moveUp();
     }
+    if (Input.isKeyDown(hxd.Key.A)) {
+        players[0].moveLeft();
+    }
+    if (Input.isKeyDown(hxd.Key.S)) {
+        players[0].moveDown();
+    }
+    if (Input.isKeyDown(hxd.Key.D)) {
+        players[0].moveRight();
+    }
+    if (Input.isKeyDown(hxd.Key.SPACE)) {
+        players[0].shoot();
+    }
+}
+
 
     static function main() {
         new Main();
