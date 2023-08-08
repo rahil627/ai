88e77b4653f69418896f538c9af451165f260afc
rathewolf <1132053+rahil627@users.noreply.github.com>
2023-08-07 23:38:38 -0700

    Create Bullet.hx

---
+++ code/shmup/heaps/Bullet.hx
@@ -0,0 +1,10 @@
+class Bullet extends h2d.Sprite {
+    public function new() {
+        super(Assets.getTexture("bullet.png"));
+        // Set initial position, speed, and other properties
+    }
+
+    public function update(dt: Float) {
+        // Handle bullet movement and collision
+    }
+}
