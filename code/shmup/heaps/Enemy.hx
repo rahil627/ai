20fbd1a4412c2ea25c2a9bd5590829f371ca8711
rathewolf <1132053+rahil627@users.noreply.github.com>
2023-08-07 23:37:54 -0700

    Create Enemy.hx

---
+++ code/shmup/heaps/Enemy.hx
@@ -0,0 +1,10 @@
+class Enemy extends h2d.Sprite {
+    public function new() {
+        super(Assets.getTexture("enemy.png"));
+        // Set initial position, speed, and other properties
+    }
+
+    public function update(dt: Float) {
+        // Handle enemy movement and behavior
+    }
+}
